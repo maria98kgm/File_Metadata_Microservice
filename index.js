9751ccc041340852fdcc7ec45bf0c6cb32c6f798
Marie Kagermanova <maria.kgm04@gmail.com>
2023-03-26 15:24:18 +0300

    make file upload possible

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const fileUpload = require("express-fileupload");
+
+require("dotenv").config();
 
 var app = express();
 
+app.use(fileUpload({ createParentPath: true }));
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", (req, res) => {
+  console.log(req.files.upfile);
+  res.json({
+    name: req.files.upfile.name,
+    type: req.files.upfile.mimetype,
+    size: req.files.upfile.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
